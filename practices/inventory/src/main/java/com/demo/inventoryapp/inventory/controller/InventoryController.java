307b6ef3ed0b3aeb0ed900f0f3d927b3419e1d89
limminjeong98 <limminjeong98@naver.com>
2024-09-26 11:52:34 +0900

    재고 차감 컨트롤러 개발 및 테스트 작성

--- practices/inventory/src/main/java/com/demo/inventoryapp/inventory/controller/InventoryController.java
+++ practices/inventory/src/main/java/com/demo/inventoryapp/inventory/controller/InventoryController.java
@@ -2,15 +2,16 @@ package com.demo.inventoryapp.inventory.controller;
 
 import com.demo.inventoryapp.common.controller.ApiResponse;
 import com.demo.inventoryapp.inventory.controller.consts.ErrorCodes;
+import com.demo.inventoryapp.inventory.controller.dto.DecreaseQuantityRequest;
 import com.demo.inventoryapp.inventory.controller.dto.InventoryResponse;
 import com.demo.inventoryapp.inventory.controller.exception.CommonInventoryHttpException;
 import com.demo.inventoryapp.inventory.service.InventoryService;
 import com.demo.inventoryapp.inventory.service.domain.Inventory;
+import com.demo.inventoryapp.inventory.service.exception.InsufficientStockException;
+import com.demo.inventoryapp.inventory.service.exception.InvalidDecreaseQuantityException;
+import com.demo.inventoryapp.inventory.service.exception.ItemNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/inventory")
 @RestController
@@ -28,6 +29,23 @@ public class InventoryController {
         if (inventory == null) {
             throw new CommonInventoryHttpException(ErrorCodes.ITEM_NOT_FOUND, HttpStatus.NOT_FOUND);
         }
+        return ApiResponse.just(InventoryResponse.fromDomain(inventory));
+    }
+
+    @PostMapping("/{itemId}/decrease")
+    ApiResponse<InventoryResponse> decreaseQuantity(@PathVariable(name = "itemId") String itemId, @RequestBody DecreaseQuantityRequest request) {
+        Inventory inventory;
+
+        try {
+            inventory = inventoryService.decreaseByItemId(itemId, request.quantity());
+        } catch (ItemNotFoundException e) {
+            throw new CommonInventoryHttpException(ErrorCodes.ITEM_NOT_FOUND, HttpStatus.NOT_FOUND);
+        } catch (InsufficientStockException e) {
+            throw new CommonInventoryHttpException(ErrorCodes.INSUFFICIENT_STOCK, HttpStatus.BAD_REQUEST);
+        } catch (InvalidDecreaseQuantityException e) {
+            throw new CommonInventoryHttpException(ErrorCodes.INVALID_DECREASE_QUANTITY, HttpStatus.BAD_REQUEST);
+        }
+
         return ApiResponse.just(InventoryResponse.fromDomain(inventory));
     }
 }
--- practices/inventory/src/test/java/com/demo/inventoryapp/inventory/controller/InventoryControllerTest.java
+++ practices/inventory/src/test/java/com/demo/inventoryapp/inventory/controller/InventoryControllerTest.java
@@ -5,6 +5,9 @@ import com.demo.inventoryapp.config.JsonConfig;
 import com.demo.inventoryapp.inventory.controller.consts.ErrorCodes;
 import com.demo.inventoryapp.inventory.service.InventoryService;
 import com.demo.inventoryapp.inventory.service.domain.Inventory;
+import com.demo.inventoryapp.inventory.service.exception.InsufficientStockException;
+import com.demo.inventoryapp.inventory.service.exception.InvalidDecreaseQuantityException;
+import com.demo.inventoryapp.inventory.service.exception.ItemNotFoundException;
 import com.demo.inventoryapp.test.fixture.InventoryFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -22,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(JsonConfig.class)
@@ -75,6 +80,101 @@ public class InventoryControllerTest {
             });
 
             verify(inventoryService).findByItemId(itemId);
+        }
+    }
+
+    @DisplayName("재고 차감")
+    @Nested
+    class DecreaseQuantity {
+        final String itemId = "1";
+        final Long quantity = 10L;
+
+        @DisplayName("자산이 존재하지 않을 경우, 404 status와 error를 반환한다")
+        @Test
+        void test1() throws Exception {
+            // given
+            given(inventoryService.decreaseByItemId(itemId, quantity))
+                    .willThrow(ItemNotFoundException.class);
+
+            // when
+            final String requestBody = "{\"quantity\": " + quantity + "}";
+            final MvcResult result = mockMvc.perform(
+                            post("/api/v1/inventory/{itemId}/decrease", itemId)
+                                    .content(requestBody)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isNotFound())
+                    .andReturn();
+
+            // then
+            assertMvcErrorEquals(result, ErrorCodes.ITEM_NOT_FOUND);
+            verify(inventoryService).decreaseByItemId(itemId, quantity);
+        }
+
+        @DisplayName("재고가 부족할 경우, 400 status와 error를 반환한다")
+        @Test
+        void test2() throws Exception {
+            // given
+            given(inventoryService.decreaseByItemId(itemId, quantity))
+                    .willThrow(InsufficientStockException.class);
+
+            // when
+            final String requestBody = "{\"quantity\": " + quantity + "}";
+            final MvcResult result = mockMvc.perform(
+                            post("/api/v1/inventory/{itemId}/decrease", itemId)
+                                    .content(requestBody)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isBadRequest())
+                    .andReturn();
+
+            // then
+            assertMvcErrorEquals(result, ErrorCodes.INSUFFICIENT_STOCK);
+            verify(inventoryService).decreaseByItemId(itemId, quantity);
+        }
+
+        @DisplayName("차감 수량이 유효하지 않은 경우, 400 status와 error를 반환한다")
+        @Test
+        void test3() throws Exception {
+            // given
+            given(inventoryService.decreaseByItemId(itemId, quantity))
+                    .willThrow(InvalidDecreaseQuantityException.class);
+
+            // when
+            final String requestBody = "{\"quantity\": " + quantity + "}";
+            final MvcResult result = mockMvc.perform(
+                            post("/api/v1/inventory/{itemId}/decrease", itemId)
+                                    .content(requestBody)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isBadRequest())
+                    .andReturn();
+
+            // then
+            assertMvcErrorEquals(result, ErrorCodes.INVALID_DECREASE_QUANTITY);
+            verify(inventoryService).decreaseByItemId(itemId, quantity);
+        }
+
+        @DisplayName("정상인 경우, 200 status와 결과를 반환한다")
+        @Test
+        void test4() throws Exception {
+            // given
+            final Inventory inventory = InventoryFixture.sampleInventory(itemId, null);
+            given(inventoryService.decreaseByItemId(itemId, quantity))
+                    .willReturn(inventory);
+
+            // when
+            final String requestBody = "{\"quantity\": " + quantity + "}";
+            final MvcResult result = mockMvc.perform(
+                            post("/api/v1/inventory/{itemId}/decrease", itemId)
+                                    .content(requestBody)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                    ).andExpect(status().isOk())
+                    .andReturn();
+
+            // then
+            assertMvcDataEquals(result, dataField -> {
+                assertEquals(inventory.itemId(), dataField.get("item_id").asText());
+                assertEquals(inventory.stock(), dataField.get("stock").asLong());
+            });
+            verify(inventoryService).decreaseByItemId(itemId, quantity);
         }
     }
 }
