4039cdba714c1941dec94888f9102e702ffb90bb
limminjeong98 <limminjeong98@naver.com>
2024-09-26 10:17:08 +0900

    Spring Boot 플러그인 의존성 관련 gradle 스크립트 작성

--- practices/inventory/build.gradle.kts
+++ practices/inventory/build.gradle.kts
@@ -1,4 +1,11 @@
 plugins {
     id("custom.java-conventions")
     id("custom.test-conventions")
+    id("custom.spring-conventions")
+}
+
+
+dependencies {
+    implementation(Spring.boot.actuator)
+    implementation(Spring.boot.web)
 }
---
+++ practices/inventory/src/test/java/com/demo/inventoryapp/inventory/controller/InventoryControllerTest.java
@@ -0,0 +1,37 @@
+package com.demo.inventoryapp.inventory.controller;
+
+import com.demo.inventoryapp.inventory.service.InventoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = InventoryController.class)
+public class InventoryControllerTest {
+
+    @MockBean
+    InventoryService inventoryService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @DisplayName("재고 조회")
+    @Nested
+    class GetStock {
+        final String itemId = "1";
+
+        @DisplayName("자산이 존재하지 않을 경우, 404 status와 error를 반환한다")
+        @Test
+        void test1() throws Exception {
+            // given
+
+            // when
+
+            // then
+            System.out.println("실행되었음");
+        }
+    }
+}
